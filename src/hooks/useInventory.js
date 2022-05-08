3a357d9db84e6229aa135b222a7e9ca6c2e5148f
hira480 <hiramannan8@gmail.com>
2022-05-08 14:32:53 +0600

    replace localhost to heroku link

--- src/hooks/useInventory.js
+++ src/hooks/useInventory.js
@@ -4,7 +4,7 @@ const useInventory = () => {
     const [inventory, setInventory] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/product')
+        fetch('https://safe-tundra-06373.herokuapp.com/product')
             .then(res => res.json())
             .then(data => setInventory(data));
     }, []);
