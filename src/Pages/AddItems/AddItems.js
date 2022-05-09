9c66b093c8f1b4ee89ab06a7f15342762a8e0dd4
hira480 <hiramannan8@gmail.com>
2022-05-09 15:07:16 +0600

    add item page implemented and connect to database

--- src/Pages/AddItems/AddItems.js
+++ src/Pages/AddItems/AddItems.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useForm } from "react-hook-form";
 
 const AddItems = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = 'https://safe-tundra-06373.herokuapp.com/product';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
+
     return (
-        <div className='container'>
+        <div>
             <h2 className='text-primary text-center mt-3'>Add Items</h2>
+            <div className=' container form-width form-layout'>
+
+                <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                    <input className='mb-3' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                    <input className='mb-3' placeholder='Price' type="number" {...register("price")} />
+                    <input className='mb-3' placeholder='Quantity' type="number" {...register("quantity")} />
+                    <input className='mb-3' placeholder='Supplier' {...register("supplier", { required: true, maxLength: 20 })} />
+                    <textarea className='mb-3' placeholder='Description' {...register("description")} />
+                    <input className='mb-3' placeholder='Photo URL' type="text" {...register("img")} />
+                    <Button className='w-100' variant="primary" type="submit">Add Item</Button>
+
+                    {/* <input type="submit" value="Add Service" /> */}
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/ManageItems/ManageItems.js
+++ src/Pages/ManageItems/ManageItems.js
@@ -2,22 +2,37 @@ import React from 'react';
 import './ManageItems.css';
 import useInventory from '../../hooks/useInventory';
 import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const ManageItems = () => {
     const [inventory, setInventory] = useInventory();
+    const navigate = useNavigate();
+    const handelAddItem = e => {
+        navigate('/addItems')
+    }
 
     return (
         <div className='container'>
-            <h2 className='text-primary text-center mt-3'>Manage Items</h2>
+            <h2 className='text-primary text-center my-3'>Manage Items</h2>
+            <div className='text-center mt-3'>
+                <Button onClick={handelAddItem} variant="outline-success">Add New Items</Button>
+            </div>
             {
                 inventory.map(product =>
-                    <div key={product._id} className='product'>
+                    <div key={product._id} className='product custom-width mx-auto'>
                         <div className='detail d-flex justify-content-between align-items-center'>
-                            <h6>{product.name}</h6>
-                            <h6>$ {product.price}</h6>
-                            <Button variant="danger">Delete</Button>
+                            <div className='d-flex justify-content-start '>
+                                <img height='80px' src={product.img} alt="" />
+                                <div className='justify-content-start align-items-center ms-3'>
+                                    <h6>{product.name}</h6>
+                                    <h6>$ <span style={{ color: 'orange' }}>{product.price}</span></h6>
+                                    <p className='mb-0'>Quantity: {product.quantity}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <Button variant="danger">Delete</Button>
+                            </div>
                         </div>
-
                     </div>)
             }
         </div>
